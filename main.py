14a94212b3c3945bf086583620620b0a801b4608
arzu00am-cloud <arzu00.am@gmail.com>
2026-01-07 22:49:40 +0400

    Update API request and replace API key

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import requests
 
-url = "https://api-football-v1.p.rapidapi.com/v3/status"
+API_KEY = "1f7e8ab5b3caabcb2a80a8dc9cb140f2"
 
-headers = {
-    "X-RapidAPI-Key": "40ac533b0bmsh65451b8e37db835p108968jsn9a7377c82036",
-    "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
-}
+r = requests.get(
+    "https://v3.football.api-sports.io/status",
+    headers={"x-apisports-key": API_KEY}
+)
 
-r = requests.get(url, headers=headers)
+print(r.status_code)
+print(r.text)import requests
 
-print("STATUS:", r.status_code)
-print("RESPONSE:", r.text)
+API_KEY = "SİZİN_RƏSMİ_API_KEY"
+
+r = requests.get(
+    "https://v3.football.api-sports.io/status",
+    headers={"x-apisports-key": API_KEY}
+)
+
+print(r.status_code)
+print(r.text)
