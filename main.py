54eb0112cacd4c4478ef123f5a238bf738264319
arzu00am-cloud <arzu00.am@gmail.com>
2026-01-07 22:29:50 +0400

    Update RapidAPI key in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 url = "https://api-football-v1.p.rapidapi.com/v3/status"
 
 headers = {
-    "X-RapidAPI-Key": "BURAYA_40_LA_BASLAYAN_KEY",
+    "X-RapidAPI-Key": "40ac533b0bmsh65451b8e37db835p108968jsn9a7377c82036",
     "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
 }
 
